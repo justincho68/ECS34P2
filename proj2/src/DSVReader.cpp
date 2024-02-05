10adb19425d462f8ecff6e836fe39b801b556b49
Justin Cho <justincho1290@gmail.com>
2024-02-04 22:01:09 -0800

    DSVReader
    Started the functions of reading lines and reading rows

--- proj2/src/DSVReader.cpp
+++ proj2/src/DSVReader.cpp
@@ -1,1 +1,32 @@
 #include "DSVReader.h"
+#include <vector>
+#include <sstream>
+#include <queue>
+
+
+struct CDSVReader::SImplementation {
+    std::shared_ptr<CDataSource> DataSource;
+    char Delimiter;
+    std::string Buffer;
+    std::queue<std::vector<std::string>> Rows;
+
+    SImplementation(std::shared_ptr<CDataSource> src) : DataSource(src), Delimiter(Delimiter) {
+    }
+
+    bool End() const {
+        return DataSource->End();
+    }
+
+    bool readLine
+    
+    bool ReadRow(std::vector< std::string > &row) {
+        if (End()) {
+            return false;
+        }
+        std::string line;
+        if(!DataSource->Read(line)) {
+            return false;
+        }
+    }
+
+};
